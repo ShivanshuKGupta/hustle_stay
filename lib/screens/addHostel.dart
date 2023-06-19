27362ba865ac0fd3a300d14330609002f8447321
Sanidhaya Sharma <sanidhyas141@gmail.com>
2023-06-19 11:08:47 +0530

    Further changes added

--- lib/screens/addHostel.dart
+++ lib/screens/addHostel.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:hustle_stay/screens/home_screen.dart';
 // import 'package:hustle_stay/models/hostels.dart';
 import '../tools.dart';
 import '../widgets/image_picker.dart';
@@ -35,6 +36,9 @@ class _AddHostelFormState extends State<AddHostelForm> {
       "capacity": capacity,
       "imageUrl": imageUrl
     });
+
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (_) => HomeScreen()));
   }
 
   File? _selectedImage;
@@ -91,110 +95,113 @@ class _AddHostelFormState extends State<AddHostelForm> {
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Form(
-          key: _formKey,
-          child: Container(
-            padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ImagePickerWidget(
-                  onpickImage: (pickedImage) {
-                    _selectedImage = pickedImage;
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(label: Text('Hostel Name')),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the hostel name.';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    hostelName = value!;
-                  },
-                ),
-                SizedBox(height: 10.0),
-                DropdownButtonFormField(
-                  decoration: InputDecoration(label: const Text('Hostel Type')),
-                  items: dropdownItems,
-                  value: 'Boys',
-                  onChanged: (value) {
-                    setState(() {
-                      hostelType = value!;
-                    });
-                  },
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please select a hostel type.';
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Capacity'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the capacity.';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Please enter a valid number.';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    capacity = int.parse(value!);
-                  },
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Number of Rooms'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the number of rooms.';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Please enter a valid number.';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    numberOfRooms = int.parse(value!);
-                  },
-                ),
-                SizedBox(height: 10.0),
-                TextFormField(
-                  decoration:
-                      InputDecoration(labelText: 'Number of Floors/Blocks'),
-                  keyboardType: TextInputType.number,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the number of floors or blocks.';
-                    }
-                    if (int.tryParse(value) == null) {
-                      return 'Please enter a valid number.';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    numberOfFloorsorBlocks = int.parse(value!);
-                  },
-                ),
-                SizedBox(height: 20.0),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  child: Text(
-                    'Add Hostel',
-                    style: TextStyle(fontSize: 16.0),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ImagePickerWidget(
+                    onpickImage: (pickedImage) {
+                      _selectedImage = pickedImage;
+                    },
                   ),
-                ),
-              ],
+                  TextFormField(
+                    decoration: InputDecoration(label: Text('Hostel Name')),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the hostel name.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      hostelName = value!;
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  DropdownButtonFormField(
+                    decoration:
+                        InputDecoration(label: const Text('Hostel Type')),
+                    items: dropdownItems,
+                    value: 'Boys',
+                    onChanged: (value) {
+                      setState(() {
+                        hostelType = value!;
+                      });
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please select a hostel type.';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Capacity'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the capacity.';
+                      }
+                      if (int.tryParse(value) == null) {
+                        return 'Please enter a valid number.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      capacity = int.parse(value!);
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Number of Rooms'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the number of rooms.';
+                      }
+                      if (int.tryParse(value) == null) {
+                        return 'Please enter a valid number.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      numberOfRooms = int.parse(value!);
+                    },
+                  ),
+                  SizedBox(height: 10.0),
+                  TextFormField(
+                    decoration:
+                        InputDecoration(labelText: 'Number of Floors/Blocks'),
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the number of floors or blocks.';
+                      }
+                      if (int.tryParse(value) == null) {
+                        return 'Please enter a valid number.';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      numberOfFloorsorBlocks = int.parse(value!);
+                    },
+                  ),
+                  SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: Text(
+                      'Add Hostel',
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
