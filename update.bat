8d46636375e0a6c4070a68cfbcbc4f92b2efa88d
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-06-21 00:42:35 +0530

    Made some changes to update.bat

--- update.bat
+++ update.bat
@@ -1,9 +1,10 @@
 @REM This script is for updating any changes to the git repo
-@echo off
 @REM Change the below name with your branch name
+@echo off
 set branchName=testbranch
 echo %branchName%
 echo "Before running this script make sure that you have a branch named %branchName% and have all your changes commited only in that branch."
+@echo on
 git add .
 git commit
 git checkout master
