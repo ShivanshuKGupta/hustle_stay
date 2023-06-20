a156019f4b48ee4e7ad076c2d5ea4b33060f12d8
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-06-21 00:39:56 +0530

    Updated 'update.bat' and added a custom command for code runner

--- update.bat
+++ update.bat
@@ -1,13 +1,15 @@
 @REM This script is for updating any changes to the git repo
 @echo off
 @REM Change the below name with your branch name
-set branchName = "sonu"
+set branchName=testbranch
 echo %branchName%
 echo "Before running this script make sure that you have a branch named %branchName% and have all your changes commited only in that branch."
+git add .
+git commit
 git checkout master
 git pull origin master
 git merge %branchName%
 git push -u origin master
-git branch %branchName%
+git checkout %branchName%
 
 @REM For any kind of conflict in merging you need to manage it manually.
