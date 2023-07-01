28a46da92431d042958999ef5f8effb57d3ce79a
Shivanshu Gupta <shivanshukgupta@gmail.com>
2023-07-01 19:55:26 +0530

    Intro screen added

---
+++ lib/screens/intro/intro_attendance.dart
@@ -0,0 +1,47 @@
+import 'package:animated_icon/animated_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:hustle_stay/tools.dart';
+
+class IntroAttendance extends StatelessWidget {
+  const IntroAttendance({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AnimateIcon(
+            animateIcon: AnimateIcons.calendarTear,
+            iconType: IconType.continueAnimation,
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              shaderText(
+                context,
+                title: "Your Hostel Attendance",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "\u2022 Never miss a beat with our smart attendance tracker\n\u2022 Track your campus presence\n\u2022 Calculate your mess refund\n\u2022 Request leaves conveniently\n\u2022 And report your arrivals in a snap",
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/intro/intro_complaints.dart
@@ -0,0 +1,47 @@
+import 'package:animated_icon/animated_icon.dart';
+import 'package:flutter/material.dart';
+import 'package:hustle_stay/tools.dart';
+
+class IntroComplaints extends StatelessWidget {
+  const IntroComplaints({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          AnimateIcon(
+            animateIcon: AnimateIcons.chatMessage,
+            iconType: IconType.continueAnimation,
+            onTap: () {},
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              shaderText(
+                context,
+                title: "Your Complaints",
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                "\u2022 Experience hassle-free complaint management with Hustle Stay.\n\u2022 Report any issues.\n\u2022 Stay updated on their status.\n\u2022 And enjoy a seamless resolution process to ensure a comfortable living environment.",
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
