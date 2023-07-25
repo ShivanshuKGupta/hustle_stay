b667954ced77acf309a466290f95cbfaa7512f7c
Sanidhaya Sharma <sanidhyas141@gmail.com>
2023-07-25 22:24:22 +0530

    bug fix

--- lib/screens/attendance_screen.dart
+++ lib/screens/attendance_screen.dart
@@ -262,7 +262,7 @@ class _AttendanceScreenState extends State<AttendanceScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         CircleAvatar(
-                            radius: screenWidth * 0.05,
+                            radius: screenWidth * 0.14,
                             child: ClipOval(
                               child: AspectRatio(
                                 aspectRatio: 1.0,
